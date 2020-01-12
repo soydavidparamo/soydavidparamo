590fd3b7b4080a64e50f88a09b0c90bbdb4fe74b
David Páramo Reina <davidparamoreina@MacBook-Pro-de-David.local>
2020-01-12 14:45:38 +0100

    Versión 2.1

--- js/hide.js
+++ js/hide.js
@@ -84,10 +84,13 @@ jQuery(document).ready(function() {
 
 window.onscroll = function() {scrollFunction()};
 
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
     document.getElementById("navbar").style.top = "0";
   } else {
     document.getElementById("navbar").style.top = "-50px";
   }
+  prevScrollpos = currentScrollPos;
 }
